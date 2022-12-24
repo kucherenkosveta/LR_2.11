bb0e16114003a9c152ec2d58c010977166cb22ec
kucherenkosveta <kucherenko55sss55@gmail.com>
2022-12-24 09:24:28 +0300

    add

---
+++ PyCharm/ind.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def max_min(type='max'):
+    def helper(collection):
+        if type == 'max':
+            return max(collection)
+        else:
+            return min(collection)
+    return helper
+
+
+if __name__ == '__main__':
+    print(max_min(input("Введите max или min: "))
+          (map(int, input("Введите список: ").split(' '))))
